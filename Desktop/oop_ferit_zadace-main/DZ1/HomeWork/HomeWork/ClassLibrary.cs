b237bba7388ee25c68f32047a170ac54b1f53106
Karlo Kraml <karlokraml2@gmail.com>
2022-10-25 22:07:25 +0200

    Homework

---
+++ Desktop/oop_ferit_zadace-main/DZ1/HomeWork/HomeWork/ClassLibrary.cs
@@ -0,0 +1,36 @@
+﻿namespace ClassLibrary
+{
+    public class Episode
+    {
+        private int viewers;
+        private double reviewSum;
+        private double highestReview;
+
+        public Episode() { viewers = 0; reviewSum = highestReview = 0; }
+        public Episode(int viewers, double reviewSum, double highestReview)
+        {
+            this.viewers = viewers;
+            this.reviewSum = reviewSum;
+            this.highestReview = highestReview;
+        }
+
+        public double GetMaxScore()
+        {
+            return highestReview;
+        }
+        public void AddView(double score)
+        {
+            viewers++;
+            reviewSum += score;
+            if (score > highestReview) highestReview = score;
+        }
+        public double GetAverageScore()
+        {
+            return reviewSum / viewers;
+        }
+        public int GetViewerCount()
+        {
+            return viewers;
+        }
+    }
+}
